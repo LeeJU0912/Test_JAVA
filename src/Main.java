1fd71b16eaf8d6deaee4d63a53f0f6f15befef23
LeeJU0912 <thirdspirit@naver.com>
2023-02-01 12:53:15 +0900

    SmartPicker

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import javax.swing.*;
+
+class FrameInit extends JFrame {
+    public FrameInit() {
+        this.setLayout(null);
+
+        JButton button = new JButton("버튼");
+        this.add(button);
+
+
+
+
+        setSize(300, 200);
+        setTitle("SmartPicker");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+}
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        FrameInit f = new FrameInit();
+
     }
 }
