031663fccfcb3d877a0721c27f0577097cf88a62
LeeJU_DESKTOP <thirdspirit@naver.com>
2023-02-16 03:17:36 +0900

    Get Solved.ac Api https://solvedac.github.io/unofficial-documentation/#/

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Test_JAVA
 
 I'm planning to make ALGO Filter PC APP
+
+# What I've learned from making this program
+
+- REST-API
+- JAVA Swing UI
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
 import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 class FrameInit extends JFrame {
     public FrameInit() {
@@ -18,9 +23,41 @@ class FrameInit extends JFrame {
     }
 
 }
+
 public class Main {
     public static void main(String[] args) {
         FrameInit f = new FrameInit();
+
+        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        // Solved.ac User Info Rest-API
+        try {
+            URL url = new URL("https://solved.ac/api/v3/user/show?handle=LJU0912");
+            HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+            httpConn.setRequestMethod("GET");
+
+            httpConn.setRequestProperty("Content-Type", "application/json");
+            httpConn.setConnectTimeout(10000);
+            httpConn.setReadTimeout(10000);
+
+            StringBuffer response = new StringBuffer();
+            String inputLine;
+            if (httpConn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(httpConn.getInputStream(), StandardCharsets.UTF_8));
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine).append("\n");
+                }
+                in.close();
+            }
+            System.out.println(response);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+
 
     }
 }
